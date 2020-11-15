edd532d987f2ab7b4c0ff51d5343d53961d38731
Documenter.jl <documenter@juliadocs.github.io>
2020-11-15 06:03:58 +0000

    build based on bbd0c14

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = RegistratorServerless","category":"page"},{"location":"#RegistratorServerless","page":"Home","title":"RegistratorServerless","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [RegistratorServerless]","category":"page"}]
+}
